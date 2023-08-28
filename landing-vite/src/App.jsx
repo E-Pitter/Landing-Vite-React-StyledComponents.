c0eb4df692a5374748f2e1f491b6f8c9857a3778
¨Pitter <barjapitter@gmail.com>
2023-08-28 15:46:43 -0300

    resolviendo error

--- landing-vite/src/App.jsx
+++ landing-vite/src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero/Hero"
 import Layout from "./components/Layout/Layout"
 import Navbar from "./components/Navbar/Navbar"
 import CardsProductos from "./components/Productos/CardsProductos"
-import Footer from "./components/footer/Footer"
+import Footer from "./components/Footer/Footer"
 
 function App() {
 
